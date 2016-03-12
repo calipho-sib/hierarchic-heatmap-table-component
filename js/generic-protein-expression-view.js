1806390ec5abb19dd5932be8078bb52a861129d9
linjinjin123 <494779656@qq.com>
2016-03-12 16:02:07 +0800

    init the dir

---
+++ js/generic-protein-expression-view.js
@@ -0,0 +1,9 @@
+/*
+* @Author: JinJin Lin
+* @Email:   jinjin.lin@outlook.com
+* @Date:   2016-03-12 15:20:55
+* @Last Modified time: 2016-03-12 16:01:12
+* All copyright reserved
+*/
+
+'use strict';
