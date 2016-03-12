1eb6cf12261547dcd57ca4a9877559194f123cdd
linjinjin123 <494779656@qq.com>
2016-03-12 23:04:51 +0800

    test gh-pages

--- js/generic-protein-expression-view.js
+++ js/generic-protein-expression-view.js
@@ -1,9 +1,18 @@
 /*
+* @File: generic-protein-expression-view.js
+* @Description: Generic protein expression view for Human Body
 * @Author: JinJin Lin
 * @Email:   jinjin.lin@outlook.com
 * @Date:   2016-03-12 15:20:55
-* @Last Modified time: 2016-03-12 16:01:12
+* @Last Modified time: 2016-03-12 20:27:48
 * All copyright reserved
 */
 
 'use strict';
+
+
+$(document).ready(function () {
+	$('label.tree-toggler').click(function () {
+		$(this).parent().children('ul.tree').toggle(300);
+	});
+});
