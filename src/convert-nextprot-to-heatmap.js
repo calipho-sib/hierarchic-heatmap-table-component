3892ffc8f7e3d294f69974a1c23b0cd5e4e6ee51
Daniel Teixeira <ddtxra@gmail.com>
2016-05-12 15:47:19 +0200

    Create convert-nextprot-to-heatmap.js

---
+++ src/convert-nextprot-to-heatmap.js
@@ -0,0 +1,4 @@
+function convertNextProtDataIntoHeatMapTable (data) {
+ //TODO convert the data here into the JSON format used by heatmap table
+ // Related task: https://trello.com/c/DNgw1yLp/12-convert-nextprot-data-into-the-json-format
+}
