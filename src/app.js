e58d57f98252ef18b0409a6e25711cdda8322ef2
Daniel Teixeira <ddtxra@gmail.com>
2016-05-12 15:45:34 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 $(function () {
 
     //TODO invoke nextprot API and and built a heatmap table for each anatomycal system instead of calling data.json mock file.
+    // Related Trello task: https://trello.com/c/DNgw1yLp/12-convert-nextprot-data-into-the-json-format
     $.getJSON("data/data.json", function(data) {
 
         var heatMapTable = new HeatMapTable({header:['Microarray', 'EST', 'IHC']});
