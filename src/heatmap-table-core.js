ad87b152b51c0b8b23e0c0d6cbf35c018cf97195
JinJin Lin <494779656@qq.com>
2016-07-03 21:20:01 +0800

    save

--- src/heatmap-table-core.js
+++ src/heatmap-table-core.js
@@ -218,7 +218,7 @@
         initInitialState: function() {
             $(".heatmap-filterByRowName-input").text("");
 
-            
+            //reset filter status
         },
 
         clear: function() {
