08fbd1f941bec39272d302d627fbca65cc8bde94
linjinjin123 <494779656@qq.com>
2016-08-07 15:43:01 +0800

    change doc's header and change bundle

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,12 +16,16 @@ module.exports = function(grunt) {
                 dest: 'dist/heatmap-table.js'
             },
             css: {
-                src: ['vendor/css/heatmap-table.css'],
+                src: ['vendor/css/heatmap-table.css',
+                      'bower_components/bootstrap/dist/css/bootstrap.min.css',
+                      'vendor/jquery.typeahead.min.css'],
                 dest: 'dist/heatmap-table.css'
             },
             vendor: {
                 src: ['bower_components/handlebars/handlebars.js',
-                      'bower_components/jquery/dist/jquery.min.js'],
+                      'bower_components/jquery/dist/jquery.min.js',
+                      'vendor/jquery.typeahead.min.js',
+                      'vendor/js/bootstrap.min.js'],
                 dest: 'dist/heatmap-table-vendor.js'
             },
             bundle: {
@@ -30,7 +34,7 @@ module.exports = function(grunt) {
                        'dist/heatmap-table.js'],
                 //the keyword bundle is for the full package source + dependencies
                 dest: 'dist/heatmap-table-bundle.js'
-            }
+            },
         },
         connect: {
             server: {
